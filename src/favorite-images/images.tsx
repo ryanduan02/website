ea87ef62904e91d7125e9d0280490a65bedfa610
Ryan Duan <ryanduan02@gmail.com>
2025-12-22 23:16:36 -0500

    add rcp photi

--- src/favorite-images/images.tsx
+++ src/favorite-images/images.tsx
@@ -1,9 +1,16 @@
 import type { FavoriteImage } from "../types/favoriteImage";
 
 export const favoriteImages: FavoriteImage[] = [
-	{
-		src: "432 Park Avenue.webp",
-		alt: "432 Park Avenue",
-		description: "NYC - 432 Park Avenue - the most beautiful skyscraper in the city",
-	},
+  {
+    src: "432 Park Avenue.webp",
+    alt: "432 Park Avenue",
+    description:
+      "NYC - 432 Park Avenue - the most beautiful skyscraper in the city",
+  },
+  {
+    src: "rcp.jpeg",
+    alt: "RCP",
+    description:
+      "Lawence, Kansas - Sports Pavillion lawrence - calm snowy night after the gym",
+  },
 ];
