4a8d17cc694cbda738e96c1b8fa55c25c9caf826
Ryan Duan <ryanduan02@gmail.com>
2025-12-22 23:35:06 -0500

    change 432 park avenue photo

--- src/favorite-images/images.tsx
+++ src/favorite-images/images.tsx
@@ -2,7 +2,7 @@ import type { FavoriteImage } from "../types/favoriteImage";
 
 export const favoriteImages: FavoriteImage[] = [
   {
-    src: "432 Park Avenue.webp",
+    src: "432 Park Avenue.jpg",
     alt: "432 Park Avenue",
     description:
       "NYC - 432 Park Avenue - the most beautiful skyscraper in the city",
