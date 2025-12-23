480e5fd9de03c5b14f6def8d81f36caa871bb8f9
Ryan Duan <ryanduan02@gmail.com>
2025-12-23 00:12:41 -0500

    decsription formatting

--- src/favorite-images/images.tsx
+++ src/favorite-images/images.tsx
@@ -5,28 +5,28 @@ export const favoriteImages: FavoriteImage[] = [
     src: "432 Park Avenue.jpg",
     alt: "432 Park Avenue",
     description:
-      "NYC - 432 Park Avenue - the most beautiful skyscraper in the city",
+      "NYC, 432 Park Avenue | the most beautiful skyscraper in the city",
       // photo source: https://pixabay.com/photos/new-york-432-park-avenue-city-6750363/
   },
   {
     src: "rcp.jpeg",
     alt: "RCP",
     description:
-      "Lawence, Kansas - Sports Pavillion Lawrence - calm snowy night after the gym",
+      "Lawence, Kansas - Sports Pavillion Lawrence | calm snowy night after the gym",
         // photo source: taken by me
   },
   {
     src: "the edge.jpeg",
     alt: "The Edge",
     description:
-      "NYC - The Edge - the best view in Manhattan",
+      "NYC, The Edge | the best view in Manhattan",
         // photo source: taken by me
   },
   {
     src: "1 and 2 Manhattan West.jpg",
     alt: "1 and 2 Manhattan West",
     description:
-      "NYC - 1 and 2 Manhattan West - New York looks like gotham when it rains",
+      "NYC, 1 and 2 Manhattan West | New York looks like gotham when it rains",
         // photo source: taken by me
   },
 ];
