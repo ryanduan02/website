7ba36b15974079854d3cce1e0a8baa3ceb82635d
Ryan Duan <ryanduan02@gmail.com>
2025-12-22 23:35:23 -0500

    add source

--- src/favorite-images/images.tsx
+++ src/favorite-images/images.tsx
@@ -6,11 +6,13 @@ export const favoriteImages: FavoriteImage[] = [
     alt: "432 Park Avenue",
     description:
       "NYC - 432 Park Avenue - the most beautiful skyscraper in the city",
+      // photo source: https://pixabay.com/photos/new-york-432-park-avenue-city-6750363/
   },
   {
     src: "rcp.jpeg",
     alt: "RCP",
     description:
       "Lawence, Kansas - Sports Pavillion lawrence - calm snowy night after the gym",
+        // photo source: taken by me
   },
 ];
